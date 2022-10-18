7553f93db4c458142892377a823b84d49e657bdf
Daniel <daniel.araujo.dev@hotmail.com>
2022-10-17 22:27:21 -0300

    Implementação nas Classes Smartphone, Nokia e Iphone.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "1234567", modelo: "1100", imei: "1234567K", 10);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone iphone = new Iphone(numero: "987654321", modelo: "6 Plus", imei: "369852147Y", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
